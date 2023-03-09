6be543e02347e00c6a3ee4e7ce74f80b235fa38c
NortheLo <louis@northe-online.de>
2023-03-09 14:44:17 +0100

    Fixed ADC bug

--- src/delay-control.c
+++ src/delay-control.c
@@ -2,94 +2,73 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
-#include "hardware/i2c.h"
 
-#include "../libs/ads1115.h"
+#include "hardware/adc.h"
+#include "hardware/gpio.h"
 
 /* 
-   PIN 15: PWM for controlling the time
+   PIN 16: PWM for controlling the time
    PIN XX: Modulation-Enable Switch
-   PIN 20: I2C SDA for the ADC with the Delay-Time, Mod-Freq and Mod-Amp pot
-   PIN 21: I2C SCL
+   PIN 26: Delay-Time potentiometer 
+   PIN 27: Mod-Freq potentiometer 
+   PIN 28: Mod-Amp potentiometer
 */
-#define I2C_FREQ 40e3
-#define I2C_PORT i2c1
-static const uint8_t ADR_ADC = 0x48; 
-static const uint8_t SDA_PIN = 14;
-static const uint8_t SCL_PIN = 15;
 
-struct ads1115_adc adc;
+#define PWM_PIN   16
+#define DTIM_PIN  26
+#define MFRQ_PIN  27
+#define MAMP_PIN  28
 
 void PWMstuff() {
-  // // Tell GPIO 0 and 1 they are allocated to the PWM
-  // gpio_set_function(15, GPIO_FUNC_PWM);
+  gpio_set_function(PWM_PIN, GPIO_FUNC_PWM);
 
-  // // Find out which PWM slice is connected to GPIO 0 (it's slice 0)
-  // uint slice_num = pwm_gpio_to_slice_num(15);
+  uint slice_num = pwm_gpio_to_slice_num(PWM_PIN);
 
-  // pwm_set_clkdiv(slice_num, 125); // pwm clock should now be running at 1MHz
+  pwm_set_clkdiv(slice_num, 125); //PWM clock should now be running at (PICO_CLOCK=125MHz/125) = 1MHz
 
-  // // Set period of 4 cycles (0 to 3 inclusive)
-  // pwm_set_wrap(slice_num, 3);
-  // // Set channel A output high for one cycle before dropping
-  // pwm_set_chan_level(slice_num, PWM_CHAN_A, 1);
+  // Set the PWM running
+  pwm_set_enabled(slice_num, true);
 
-  // // Set initial B output high for three cycles before dropping
-  // pwm_set_chan_level(slice_num, PWM_CHAN_B, 3);
+  pwm_set_wrap(slice_num, 1e3);
+  // Set channel A output high for one cycle before dropping
+  pwm_set_chan_level(slice_num, PWM_CHAN_A, 500);
+}
 
-  // // Set the PWM running
-  // pwm_set_enabled(slice_num, true);
+void setupADC() {
+  adc_init();
+  adc_gpio_init(DTIM_PIN);
+  adc_gpio_init(MFRQ_PIN);
+  adc_gpio_init(MAMP_PIN);
+}
 
-  // Note we could also use pwm_set_gpio_level(gpio, x) which looks up the
-  // correct slice and channel for a given GPIO.
+void adcLoop(uint16_t* adcData) {
+  int cnt = 0;
+  adc_select_input(cnt);
+
+  while (true)
+  {
+    *(adcData+cnt) = adc_read();
+
+    //printf("Ch %d: %u\n", cnt, *(adcData + cnt));
+    cnt++;
+    if (cnt > 2) {
+      cnt = 0;
+    }
+    adc_select_input(cnt);
+    
+    sleep_ms(100);
+  }
 }
 
 int main() {
+  uint16_t dat[2] = {0};
   stdio_init_all();
+  setupADC();
 
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+  gpio_put(PICO_DEFAULT_LED_PIN, 1);
 
-    // Initialise I2C
-    i2c_init(I2C_PORT, I2C_FREQ);
-    gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(SDA_PIN);
-    gpio_pull_up(SCL_PIN);
-
-    // Initialise ADC
-    ads1115_init(I2C_PORT, ADR_ADC, &adc);
-    
-    // and the full-scale voltage range is set to +/- 4.096 V.
-    ads1115_set_input_mux(ADS1115_MUX_SINGLE_1, &adc);
-    ads1115_set_pga(ADS1115_PGA_4_096, &adc);
-    ads1115_set_data_rate(ADS1115_RATE_475_SPS, &adc);
-
-    // Write the configuration for this to have an effect.
-    ads1115_write_config(&adc);
-
-    // Data containers
-    float volts;
-    uint16_t adc_value;
-
-    while (true) {
-        ads1115_set_input_mux(ADS1115_MUX_SINGLE_0, &adc);
-        // Read a value, convert to volts, and print.
-        ads1115_read_adc(&adc_value, &adc);
-        volts = ads1115_raw_to_volts(adc_value, &adc);
-        printf("ADC Ch-0: %u  Voltage: %f\n", adc_value, volts);
-
-        ads1115_set_input_mux(ADS1115_MUX_SINGLE_1, &adc);
-        // Read a value, convert to volts, and print.
-        ads1115_read_adc(&adc_value, &adc);
-        volts = ads1115_raw_to_volts(adc_value, &adc);
-        printf("ADC Ch-1: %u  Voltage: %f\n", adc_value, volts);
-
-        ads1115_set_input_mux(ADS1115_MUX_SINGLE_2, &adc);
-        // Read a value, convert to volts, and print.
-        ads1115_read_adc(&adc_value, &adc);
-        volts = ads1115_raw_to_volts(adc_value, &adc);
-        printf("ADC Ch-2: %u  Voltage: %f\n", adc_value, volts);
-
-        sleep_ms(10);
-    }
-
+  adcLoop(dat);
+  gpio_put(PICO_DEFAULT_LED_PIN, 0);
 }
